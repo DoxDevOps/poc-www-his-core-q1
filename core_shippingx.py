12c010ca25866b8bf0f1ea21d0a502be4f3e873e
Billz <58287516+billz94@users.noreply.github.com>
2022-07-21 20:09:51 +0200

    Update core_shippingx.py

--- core_shippingx.py
+++ core_shippingx.py
@@ -76,13 +76,13 @@ for site_id in cluster['site']:
             core_version = "v1.2.12"
             
             if core_version == version:
-                msgx = "Hi there,\n\nDeployment of HIS-Core to " + version + " for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
+                msgx = "Hi there,\n\nDeployment of HIS-Core to " + version + " for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF/LIN HIS."
             else:
-                msgx = "Hi there,\n\nSomething went wrong while checking out to the latest HIS-Core version. Current version is " + version + " for " + site['name'] + ".\n\nThanks!\nEGPAF HIS."
+                msgx = "Hi there,\n\nSomething went wrong while checking out to the latest HIS-Core version. Current version is " + version + " for " + site['name'] + ".\n\nThanks!\nEGPAF/LIN HIS."
 
             # send sms alert
             for recipient in recipients:
-                msg = "Hi there,\n\nDeployment of HIS-Core to " + version + " for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF HIS."
+                msg = "Hi there,\n\nDeployment of HIS-Core to " + version + " for " + site['name'] + " completed succesfully.\n\nThanks!\nEGPAF/LIN HIS."
                 params = {
                     "api_key": os.getenv('API_KEY'),
                     "recipient": recipient,
@@ -100,7 +100,7 @@ for site_id in cluster['site']:
             # make sure we are sending the alert at the last pint attempt
             if count == 3:
                 for recipient in recipients:
-                    msg = "Hi there,\n\nDeployment of HIS-Core to v1.2.12 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
+                    msg = "Hi there,\n\nDeployment of HIS-Core to v1.2.12 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF/LIN HIS."
                     params = {
                         "api_key": os.getenv('API_KEY'),
                         "recipient": recipient,
